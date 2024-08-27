e2a5a4a40290dd1bec864703304f0b12fe22fbe3
musabismael <musabismael99@gmail.com>
2024-08-27 22:26:36 +0400

    add key

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['24aac9375669caad409098c53f4694e21a9d651de32cf50ef17c3d282df6a730']
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
