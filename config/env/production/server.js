5fe0c12c826d9d5d9fc1e322ef5600ccc0b23966
musabismael <musabismael99@gmail.com>
2024-08-27 22:15:25 +0400

    "Update APP_KEYS configuration to use process.env.APP_KEYS.split(',') instead of env.array('APP_KEYS')"

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,6 @@ export default ({ env }) => ({
     proxy: true,
     url: env('APP_URL'), // Sets the public URL of the application.
     app: { 
-      keys: env.array('APP_KEYS')
+      keys: process.env.APP_KEYS.split(','),
     },
 });
