ccb3ab96ddcb8574875044e6ef4b98901aa46caf
musabismael <musabismael99@gmail.com>
2024-08-27 22:55:46 +0400

    replace admin

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT','o1+lOYEZp8o/jd81jCf4AQ=='),
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
