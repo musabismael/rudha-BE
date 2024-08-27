903bf6e0c39198c1cfebd1b756600bb9d9011e33
musabismael <musabismael99@gmail.com>
2024-08-27 22:21:30 +0400

    update changes

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,6 @@ export default ({ env }) => ({
     proxy: true,
     url: env('APP_URL'), // Sets the public URL of the application.
     app: { 
-      keys: process.env.APP_KEYS.split(','),
+      keys: env.array('APP_KEYS')
     },
 });
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: process.env.APP_KEYS.split(','),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
