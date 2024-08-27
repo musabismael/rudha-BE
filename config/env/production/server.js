368c2ac3d6fa60ea0c71c8f3cdc560b621b63a6b
musabismael <musabismael99@gmail.com>
2024-08-27 22:57:45 +0400

    get chnages

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,6 @@ export default ({ env }) => ({
     proxy: true,
     url: env('APP_URL'), // Sets the public URL of the application.
     app: { 
-      keys: ['24aac9375669caad409098c53f4694e21a9d651de32cf50ef17c3d282df6a730']
+      keys: env.array('APP_KEYS')
     },
 });
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ['24aac9375669caad409098c53f4694e21a9d651de32cf50ef17c3d282df6a730']
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
